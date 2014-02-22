2fcdd6c82f3f0b422887662641779f21950f3a86
Thomas Kollbach <thomas@kollba.ch>
2014-02-22 14:57:10 +0100

    Initial Commit

---
+++ CoreDataController/CoreDataController.m
@@ -0,0 +1,13 @@
+//
+//  CoreDataController.m
+//  CoreDataController
+//
+//  Created by Thomas Kollbach on 22.02.14.
+//  Copyright (c) 2014 nxtbgthng GmbH. All rights reserved.
+//
+
+#import "CoreDataController.h"
+
+@implementation CoreDataController
+
+@end
